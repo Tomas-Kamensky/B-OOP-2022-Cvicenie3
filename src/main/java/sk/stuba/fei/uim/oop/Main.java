c2cc6d86412f0a53be9e0d0fe51df71f8ac511d9
kamenskyt <kamenskyt@emminter.sk>
2022-03-06 11:15:47 +0100

    getter, setter

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -5,14 +5,18 @@ import sk.stuba.fei.uim.oop.auto.Auto;
 public class Main {
     public static void main(String[] args) {
         Auto skoda = new Auto();
-        Auto bmw = new Auto();
 
-        skoda.jedna = 1;
-        bmw.jedna = 2;
+        skoda.setStavNadrze(100);
+        skoda.setKapacitaNadrze(100);
+        System.out.println(skoda.stav());
 
-        if(skoda.jedna < bmw.jedna){
-            System.out.println("skoda je mensia");
-        }
+
+
+        skoda.jazdi(100);
+        System.out.println(skoda.stav());
+
+        skoda.jazdi(200);
+        System.out.println(skoda.stav());
 
 
 
--- src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
+++ src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
@@ -1,15 +1,53 @@
 package sk.stuba.fei.uim.oop.auto;
 
 public class Auto {
-    int pocetKolies;
-    int pocetDveri;
-    String nazov;
+    public static final double PALIVO_NA_KM = 0.2;
 
-    public int jedna;   //public
-    int dve;            //package private
-    protected int tri;  //suvisi s dedenim
-    private int styri;  //private
+    private double kapacitaNadrze;
+    private double stavNadrze;
+    private boolean ojazdene;
+
+    public void jazdi(double vzdialenost){
+        this.ojazdene = true;
+        stavNadrze -= vzdialenost*PALIVO_NA_KM;
+        if(stavNadrze < 0){
+            stavNadrze = 0;
+        }
+    }
+
+    public String stav(){
+        String vysledok;
+        if(this.ojazdene){
+            vysledok = "Auto je ojazdene";
+        }else{
+            vysledok = "Auto je nove";
+        }
+        vysledok += ", stav nadrze: " + stavNadrze + "/" + kapacitaNadrze;
+        return vysledok;
+    }
 
 
+    public void setKapacitaNadrze(double kapacitaNadrze) {
+        this.kapacitaNadrze = kapacitaNadrze;
+    }
 
+    public double getKapacitaNadrze() {
+        return kapacitaNadrze;
+    }
+
+    public double getStavNadrze() {
+        return stavNadrze;
+    }
+
+    public void setStavNadrze(double stavNadrze) {
+        this.stavNadrze = stavNadrze;
+    }
+
+    public boolean isOjazdene() {
+        return ojazdene;
+    }
+
+    public void setOjazdene(boolean ojazdene) {
+        this.ojazdene = ojazdene;
+    }
 }
